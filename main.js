50ad97063f02513621db3212507fe767e1e2b088
LuizGabrielOp <84206181+LuizGabrielOp@users.noreply.github.com>
2024-06-13 15:31:28 -0300

    inputs validate

--- main.js
+++ main.js
@@ -1,10 +1,45 @@
 const form = document.getElementById('form')
 const inputs = document.querySelectorAll('.required')
-const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.[a-z]?$/i
+const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]{2,}$/i;
 const span = document.querySelectorAll('.span-required')
+
+function setError(index){
+    inputs[index].style.border = '2px solid #FF0000'
+    span[index].style.display = 'block'
+}
+
+function noError(index){
+    inputs[index].style.border = 'none'
+    span[index].style.display = 'none'
+}
 
 function nameValidate(){
     if(inputs[0].value.length < 3){
-        alert('valor invalido')
-    }else{}
+        setError(0);
+    }else{
+        noError(0);
+    }
+}
+
+function emailValidate(){
+    if(emailRegex.test(inputs[1].value)){
+        noError(1);
+    }else{
+        setError(1);
+    }
+}
+
+function passwordValidate(){
+    if(inputs[2].value.length < 8){
+        setError(2);
+    }else{
+        noError(2);
+    }
+}
+function repeatPassValidate(){
+    if(inputs[3].value == inputs[2].value){
+        noError(3);
+    }else{
+        setError(3);
+    }
 }
